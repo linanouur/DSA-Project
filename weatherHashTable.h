fb080bfc8410bef1ce93553679300e1aa32d1b31
demmouchelina <146681358+demmouchelina@users.noreply.github.com>
2023-12-08 17:26:33 +0100

    Add files via upload

---
+++ weatherHashTable.h
@@ -0,0 +1,20 @@
+#include"w_year.h"
+using namespace std;
+
+
+class weatherHashTable {
+private:
+    std::vector<year> years;
+    int startYear; 
+
+public:
+    weatherHashTable(int startYear,int numberOfYears );
+
+    void addYear(int year);
+
+    weather& getWeather(int year, int month, int day);
+
+    void insertWeather(int year, int month, int day, const weather& weather) ;
+
+    int getsize(){return years.size();}
+};

8a62a7ab98578f7ac5a7a708f6d741bacd44b312
Slama Lina Nour <lina.slama@ensia.edu.dz>
2023-12-10 17:42:26 +0100

    Update customer.cpp

--- customer.cpp
+++ customer.cpp
@@ -223,14 +223,14 @@ int Customer ::generateCustomerID()
 int main()
 {
     Customers BST;
-    BST.insertNewCustomer("John", "Doe", 12345, 54321, 3, "North", "Mahelma", "Bouira");
-    BST.insertNewCustomer("Jane", "Doe", 67890, 21345, 1, "South", "Algiers", "Bab El Oued");
-    BST.insertNewCustomer("Michael", "Smith", 45678, 87654, 4, "East", "Constantine", "Salah Bey");
-    BST.insertNewCustomer("Emma", "Brown", 23456, 12345, 2, "West", "Oran", "Es Senia");
-    BST.insertNewCustomer("David", "Miller", 89012, 98765, 5, "North", "Skikda", "El Harrouch");
-    BST.insertNewCustomer("Aisha", "Boudjemaa", 76543, 89012, 2, "South", "Tlemcen", "Nedroma");
-    BST.insertNewCustomer("Karim", "Belkacem", 34567, 56789, 4, "East", "Batna", "Merouana");
-    BST.insertNewCustomer("Fatima", "Zohra", 12345, 34567, 1, "West", "Annaba", "El Bouni");
+    BST.insertNewCustomer("John", "Doe", 12345, 3, "North", "Mahelma", "Bouira");
+    BST.insertNewCustomer("Jane", "Doe", 67890, 1, "South", "Algiers", "Bab El Oued");
+    BST.insertNewCustomer("Michael", "Smith", 45678, 4, "East", "Constantine", "Salah Bey");
+    BST.insertNewCustomer("Emma", "Brown", 23456, 2, "West", "Oran", "Es Senia");
+    BST.insertNewCustomer("David", "Miller", 89012, 5, "North", "Skikda", "El Harrouch");
+    BST.insertNewCustomer("Aisha", "Boudjemaa", 76543, 2, "South", "Tlemcen", "Nedroma");
+    BST.insertNewCustomer("Karim", "Belkacem", 34567, 4, "East", "Batna", "Merouana");
+    BST.insertNewCustomer("Fatima", "Zohra", 12345, 1, "West", "Annaba", "El Bouni");
     BST.print(); // Outputs the ElectricityAccountId values of the customers
     cout << endl;
 

c1b34e64d4516b870f37f8de071180e5c39665a7
Wissam Lakhdari <wissam.lakhdari@ensia.edu.dz>
2023-12-12 17:40:24 +0100

    CustomersCSV

--- customers.cpp
+++ customers.cpp
@@ -128,38 +128,40 @@ void Customers ::displayWinner()
 
 int main()
 {
-    Customers BST;
-    BST.insertNewCustomer("John", "Doe", 12345, 3, "North", "Mahelma", "Bouira");
-    BST.insertNewCustomer("Jane", "Doe", 67890, 1, "South", "Algiers", "Bab El Oued");
-    BST.insertNewCustomer("Michael", "Smith", 45678, 4, "East", "Constantine", "Salah Bey");
-    BST.insertNewCustomer("Emma", "Brown", 23456, 2, "West", "Oran", "Es Senia");
-    BST.insertNewCustomer("David", "Miller", 89012, 5, "North", "Skikda", "El Harrouch");
-    BST.insertNewCustomer("Aisha", "Boudjemaa", 76543, 2, "South", "Tlemcen", "Nedroma");
-    BST.insertNewCustomer("Karim", "Belkacem", 34567, 4, "East", "Batna", "Merouana");
-    BST.insertNewCustomer("Fatima", "Zohra", 12345, 1, "West", "Annaba", "El Bouni");
-    BST.print(); // Outputs the ElectricityAccountId values of the customers
-    cout << endl;
+    // Customers BST;
+    // BST.insertNewCustomer("John", "Doe", 12345, 3, "North", "Mahelma", "Bouira");
+    // BST.insertNewCustomer("Jane", "Doe", 67890, 1, "South", "Algiers", "Bab El Oued");
+    // BST.insertNewCustomer("Michael", "Smith", 45678, 4, "East", "Constantine", "Salah Bey");
+    // BST.insertNewCustomer("Emma", "Brown", 23456, 2, "West", "Oran", "Es Senia");
+    // BST.insertNewCustomer("David", "Miller", 89012, 5, "North", "Skikda", "El Harrouch");
+    // BST.insertNewCustomer("Aisha", "Boudjemaa", 76543, 2, "South", "Tlemcen", "Nedroma");
+    // BST.insertNewCustomer("Karim", "Belkacem", 34567, 4, "East", "Batna", "Merouana");
+    // BST.insertNewCustomer("Fatima", "Zohra", 12345, 1, "West", "Annaba", "El Bouni")
+    // ;
+    // BST.print();  
+    // cout << endl;
 
-    cout << "enter customer id" << endl;
-    int id;
-    cin >> id;
-    cout << getRegionId(id) << endl;
-    cout << getCityId(id) << endl;
-    cout << getDistrictId(id) << endl;
-    cout << getCustomerID(id) << endl;
+    // cout << "enter customer id" << endl;
+    // int id;
+    // cin >> id;
+    // cout << getRegionId(id) << endl;
+    // cout << getCityId(id) << endl;
+    // cout << getDistrictId(id) << endl;
+    // cout << getCustomerID(id) << endl;
 
-    Customer *cust = BST.searchCustomer(67890);
-    if (cust != nullptr)
-    {
-        cust->Customeryears->insertYear(1950);
-        Year &y = cust->Customeryears->getYear(1950);
-        Bill &m = y.yearMonths->getbill(3);
-        cout << "Month number: " << m.numberMonth << endl;
-    }
-    else
-    {
-        cout << "Customer not found." << endl;
-    }
+    // Customer *cust = BST.searchCustomer(67890);
+    // if (cust != nullptr)
+    // {
+    //     cust->Customeryears->insertYear(1950);
+    //     Year &y = cust->Customeryears->getYear(1950);
+    //     Bill &m = y.yearMonths->getbill(3);
+    //     cout << "Month number: " << m.numberMonth << endl;
+    // }
+    // else
+    // {
+    //     cout << "Customer not found." << endl;
+    // }
+    cout<<"hello world";
     return 0;
 }
 

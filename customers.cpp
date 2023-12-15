14f56307c838c6f58d965d526ca96882ac8105d0
Slama Lina Nour <lina.slama@ensia.edu.dz>
2023-12-15 14:14:40 +0100

    Update customers.cpp

--- customers.cpp
+++ customers.cpp
@@ -12,7 +12,8 @@
 #include "userFunctions.cpp"
 #include <fstream>
 #include <sstream>
-#include <vector>
+#include <vector> 
+#include <iomanip> 
 
 using namespace std;
 
@@ -128,8 +129,9 @@ void Customers ::displayWinner()
 int main()
 {
     Customers BST;
-    BST.insertNewCustomer("John", "Doe", 12345, 3, "North", "Mahelma", "Bouira");
-    BST.insertNewCustomer("Jane", "Doe", 67890, 1, "South", "Algiers", "Bab El Oued");
+    BST.insertNewCustomer("John", "Doe", 12345, 3, "Adrar", "Adrar", "Adrar");
+    BST.insertNewCustomer("Jane", "Doe", 67890, 1, "Adrar", "Adrar", "Ouled Ahmed Timmi");  
+    
     BST.insertNewCustomer("Michael", "Smith", 45678, 4, "East", "Constantine", "Salah Bey");
     BST.insertNewCustomer("Emma", "Brown", 23456, 2, "West", "Oran", "Es Senia");
     BST.insertNewCustomer("David", "Miller", 89012, 5, "North", "Skikda", "El Harrouch");
@@ -137,15 +139,20 @@ int main()
     BST.insertNewCustomer("Karim", "Belkacem", 34567, 4, "East", "Batna", "Merouana");
     BST.insertNewCustomer("Fatima", "Zohra", 12345, 1, "West", "Annaba", "El Bouni");
     BST.print(); // Outputs the ElectricityAccountId values of the customers
-    cout << endl;
+    
 
     cout << "enter customer id" << endl;
     int id;
-    cin >> id;
-    cout << getRegionId(id) << endl;
-    cout << getCityId(id) << endl;
-    cout << getDistrictId(id) << endl;
-    cout << getCustomerID(id) << endl;
+    cin >> id;  
+   
+   
+    cout<<getCustomerID(id)<<endl;  
+
+    cout<<getDistrictId(id)<<endl;
+    cout<<getCityId(id)<<endl;
+    cout<<getRegionId(id)<<endl;
+   
+
 
     Customer *cust = BST.searchCustomer(67890);
     if (cust != nullptr)

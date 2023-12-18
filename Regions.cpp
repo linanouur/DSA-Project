ef57fddd5aafb406194150dc329d6ab9d81ed69f
Wissam Lakhdari <wissam.lakhdari@ensia.edu.dz>
2023-12-18 12:00:43 +0100

    change

--- Regions.cpp
+++ Regions.cpp
@@ -9,6 +9,7 @@ using namespace std;
 #include "customers.h"
 #include "customers.cpp"
 #include "customer.h"
+#include"customer.cpp"
 #include "calendar.cpp"
 #include "bills.h"
 Region::Region() : RegionID(0), RegionName("")
@@ -58,28 +59,3 @@ void htRegions::insertDistrict(int regionID, int cityID, const District &distric
     city->Districts->insertDistrict(district);
 }
 
-int main()
-{
-    htRegions regionHashTable;
-
-    Region region1(1, "Region1");
-    Region region2(2, "Region2");
-    regionHashTable.insertRegion(region1);
-    htDistricts districtHashTable;
-    District district1(1, "District1");
-    District district2(2, "District2");
-    District district3(3, "District3");
-    District district4(4, "District4");
-    htCities cityHashTable;
-    City city1(1, "City1");
-    City city2(2, "City2");
-    regionHashTable.insertCity(1, city1);
-    regionHashTable.insertCity(2, city2);
-    regionHashTable.insertDistrict(1, 1, district1);
-    regionHashTable.insertDistrict(1, 1, district2);
-    regionHashTable.insertDistrict(2, 1, district3);
-    regionHashTable.insertDistrict(2, 2, district4);
-    regionHashTable.displaycities(1);
-
-    return 0;
-}

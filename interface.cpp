0704315184ff1adeca687f734cb1b3667afcdc41
Wissam Lakhdari <wissam.lakhdari@ensia.edu.dz>
2023-12-23 18:36:49 +0100

    .

--- interface.cpp
+++ interface.cpp
@@ -6,15 +6,15 @@ using namespace std;
 
 int main()
 {
-    cout << "Loading.." << endl;
+    cout << "Electricity Management system loading.." << endl;
     htRegions Alg;
     DepartmentHeap AlgHeap;
-    FillHashTablesRCD(Alg,AlgHeap);
-   // SetCustomersFromFile(Alg);
-    FillHashTablesRCD(Alg,AlgHeap);
+    FillHashTablesRCD(Alg, AlgHeap);
+    SetCustomersFromFile(Alg);
+    FillHashTablesRCD(Alg, AlgHeap);
     try
     {
-        menu m(Alg,AlgHeap);
+        menu m(Alg, AlgHeap);
         m.display();
         m.HomePage();
         return 0;
